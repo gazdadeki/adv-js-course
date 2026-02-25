29d5cc81d0558f8a1ece32112598a7fd4ebd4941
dblanarik991 <dejan.blanarik@panonit.com>
2026-02-25 12:50:19 +0100

    execution context lesson

--- lesson_2/execution-context-lifecycle.js
+++ lesson_2/execution-context-lifecycle.js
@@ -2,20 +2,35 @@
 debugger;
 console.log("=== Global execution start ===");
 console.log(globalValue, 'globalValue');
+<<<<<<< Updated upstream
 console.log(outer, 'outer in memory before declaration');
+=======
+>>>>>>> Stashed changes
 
 var globalValue = 2;
 
 function outer(a) {
+  debugger;
   var result = a + a;
 
-  debugger;
+  console.log('inner function declaration', inner);
+  function inner(b) {
+    debugger
+
+    console.log('innerResult before declaration', innerResult)
+    var innerResult = b * b
+
+    return innerResult;
+  }
+
+  inner(result)
 
   return result;
 }
 
 var result = outer(globalValue);
 var result_two = outer(4);
+
 
 console.log("Global Variable Value:", globalValue);
 console.log("Result:", result);
